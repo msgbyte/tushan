12d125bd8df4db4900077185cc2c20cb5ee2c16d
moonrailgun <moonrailgun@gmail.com>
2023-05-16 15:56:50 +0800

    feat: express template add build script

--- packages/create-tushan/templates/express/src/server/index.ts
+++ packages/create-tushan/templates/express/src/server/index.ts
@@ -9,6 +9,12 @@ app.get('/hello', (_, res) => {
   res.send('Hello Vite + React + TypeScript!');
 });
 
+if (process.env.NODE_ENV === 'production') {
+  ViteExpress.config({
+    mode: 'production',
+  });
+}
+
 ViteExpress.listen(app, port, () =>
   console.log(
     `Server is listening on port ${port}, visit with: http://localhost:${port}`
