64c129b29a4c03b5ad7a85a7eaa0a1d590b06687
moonrailgun <moonrailgun@gmail.com>
2023-05-30 15:08:38 +0800

    chore: add umami script

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -59,6 +59,15 @@ const config = {
     require.resolve('docusaurus-plugin-image-zoom'),
   ],
 
+  scripts: [
+    {
+      src: 'https://umami.moonrailgun.com/umami.js',
+      async: true,
+      defer: true,
+      'data-website-id': 'f43cbaec-4a43-4697-989f-3615b8dc489b',
+    },
+  ],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
