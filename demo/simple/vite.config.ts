735ad8ff2385da3c70f324501cf5cb442081562c
moonrailgun <moonrailgun@gmail.com>
2023-06-02 15:17:45 +0800

    chore: add demo user/pass tip

--- demo/simple/vite.config.ts
+++ demo/simple/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+console.log('后台默认密码: tushan/tushan');
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
