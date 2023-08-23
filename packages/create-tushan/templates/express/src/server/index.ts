e6e48b946af61e4e391a709cbfa0e16c9eb1899a
moonrailgun <moonrailgun@gmail.com>
2023-08-23 16:57:45 +0800

    refactor: update templates for express

--- packages/create-tushan/templates/express/src/server/index.ts
+++ packages/create-tushan/templates/express/src/server/index.ts
@@ -5,8 +5,15 @@ const app = express();
 
 const port = Number(process.env.PORT || 3000);
 
+app.use(express.json());
+
 app.get('/hello', (_, res) => {
   res.send('Hello Vite + React + TypeScript!');
+});
+
+app.use((err: any, req: any, res: any, next: any) => {
+  res.status(500);
+  res.json({ error: err.message });
 });
 
 if (process.env.NODE_ENV === 'production') {
